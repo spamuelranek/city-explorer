3ea1ecd966fea18e8951bb95b17a383d6884890d
Samuel Panek <samuelrpanek@gmail.com>
2021-11-03 16:35:57 -0700

    adds weather api data acceptance

--- src/Main.js
+++ src/Main.js
@@ -25,65 +25,62 @@ export default class Main extends Component{
     // needs to capture value of input form and set the state of the main component    
     handleClick = () =>{
         this.requestLocation();
-        this.setState({showCity:true});
 
     }
     
     //create function that then uses new state of city to request from LocationIQ
     requestLocation = async () => {
         let url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATION_KEY}&q=${this.state.cityValue}&format=json`;
+        console.log(url);
         try{
 
+            console.log('try');
             //request from locationiq
             let response = await axios.get(url);
+
+            console.log(response);
             
             //captured response object from array response
             let returnedResponse = response.data[0];
     
-            this.returnedLocation(returnedResponse);
+            this.setState({searchedCity:returnedResponse});
+            
             this.requestForecast();
         } catch (e) {
-            let modifiedResponse = e.response.status;
-            console.log(modifiedResponse);
-            this.setState({error: true});
-            this.setState({errorStatus:modifiedResponse});
+            if(e){
+                console.log(e.response)
+                this.setState({error: false});
+            }
         }
     }
 
-    // this will send to our server to gather forecast 
+    // create request for weather data
     requestForecast = async() =>{
-        let cityName = this.manipulateDiplayName(this.state.searchedCity.display_name);
 
 
-
-            let url = `http://localhost:3001/weather?city_name=${cityName}&lat=${this.state.searchedCity.lat}&lon=${this.state.searchedCity.lon}`;
-    
+        try{
+            //create url from the searched city
+            let url = `http://localhost:3001/weather?lat=${this.state.searchedCity.lat}&lon=${this.state.searchedCity.lon}`;
+        
+            //send the request to the server
             let response = await axios.get(url);
-    
+        
             console.log(response.data);
-    
+        
             let forecastData = response.data
-    
+        
             this.setState({forecast:forecastData})
-    
+        
             console.log(this.state.forecast);
+
         }
-
-
-
-    // this will make the name work for the search query
-    manipulateDiplayName = (displayName) => {
-        let split = displayName.split(',');
-        let splice = split.splice(0,1);
-        let displayCityName = splice[0];
-        return displayCityName;
-
+        catch(e){
+            if(e){
+                console.error('something went wrong');
+            }
+        }
     }
 
-    //sets the searchedCity state from the returned data
-    returnedLocation = (returnedCities) =>{
-        this.setState({searchedCity:returnedCities});
-    }
 
     // creates function to pass to error pop up to be able to close modal
     closeErrorModal = () =>{
@@ -95,8 +92,8 @@ export default class Main extends Component{
             <main>
                 <ErrorPopUp closeModal = {this.closeErrorModal} errorStatus={this.state.errorStatus} show={this.state.error}/>
                 <SearchForm handleClick = {this.handleClick} handleOnChange ={this.onChangeOfInput} input ={this.state.cityValue}/>
-                <ReturnCityData displayCard = {this.state.lat} city = {this.state.searchedCity} map = {this.state.mapImage}/>
-                <Weather forecast = {this.state.forecast}/>
+                {this.state.forecast && <ReturnCityData displayCard = {this.state.lat} city = {this.state.searchedCity}/>}
+                {this.state.forecast && <Weather forecast = {this.state.forecast}/>}
             </main>
 
         )
--- src/Weather.js
+++ src/Weather.js
@@ -1,17 +1,24 @@
 import { Component } from "react";
 import Card from 'react-bootstrap/Card';
 
+
 export default class Weather extends Component{
+
+    hourManage = (element) =>{
+        let split = element.split('T');
+        let hour = split[1];
+        return hour;
+    }
 
     render(){
         return(
             <div className="forecastCards">
-                {this.props.forecast.map(element =>{
+                {this.props.forecast && this.props.forecast.map(element =>{
                     return (
-                        <Card key = {element.date}>
+                        <Card key = {element.hour}>
                             <Card.Body>
-                                <Card.Title>{element.date}</Card.Title>
-                                <Card.Text>{element.description}</Card.Text>
+                                <Card.Title>{this.hourManage(element.hour)}</Card.Title>
+                                <Card.Text>Temp: {element.temp}</Card.Text>
                             </Card.Body>
                         </Card>
                     )
